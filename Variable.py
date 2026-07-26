de4f57ab0773bdf54e4d8578d1088c91cbf68e7d
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-07-26 13:10:39 +0600

    Variables

---
+++ Variable.py
@@ -0,0 +1,27 @@
+x=100
+print(type(x))
+
+
+x='fahim'
+print(type(x))
+
+
+X=3 * 3.14
+print(type(X))
+print(X)
+
+
+
+X=[10,20,30]
+print(type(X))
+print(X)
+print(X[2])
+X[2]=50
+print(X[2])
+print(X)
+
+X=(10,20,30)
+print(type(X))
+print(X)
+print(X[2])
+# X[2]=70     # This will cause an error because tuples are immutable
