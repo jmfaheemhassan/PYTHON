399f67d026f4b1cb7317dd9cc6bd3e4c14b90c26
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-07-31 21:52:56 +0600

    Datatypes

---
+++ Datatypes.py
@@ -0,0 +1,123 @@
+#python numbers :
+
+
+import math
+
+
+num = 10 
+type (num)
+print(type(num))
+
+
+
+
+num = 10.5
+type(num)
+print(type(num))                
+
+
+
+#complex numbers:
+
+
+num = 2+3j
+type(num)
+print(type(num))
+
+
+
+#Arthmetic noperation 
+
+num1=10
+num2=5
+print(num1+num2)
+print(num1-num2)
+print(num1*num2)
+print(num1/num2)  
+print(num1%num2)
+print(num1**num2)
+print(num1//num2)
+
+
+#conversions
+
+
+x=192 # it is int but ,
+x="192" # it is string now 
+
+
+x="192"
+type(x)
+print(type(x))
+x=int(x)
+type(x)
+print(type(x))
+
+X="192.5"
+X=float(X)
+type(X)
+print(type(X))
+print(X)
+X=complex(X)
+print(X)
+print(complex(2,3))
+
+
+
+
+
+
+#### Function 
+
+#absulate function 
+
+x=-7.5
+type(x)
+print(abs(x))
+print(type(x))
+
+#exponent function 
+#exp is present wiyhin the math library 
+import math 
+X=10
+print(math.exp(X))
+
+math.exp(10)
+math.pi 
+print(math.pi)
+math.sqrt
+print(math.sqrt(25))
+
+
+numbers=(23,50,700)
+print(max(numbers))
+print(min(numbers))
+print(type(numbers))
+
+
+#Whta are list :
+# A list is a collection of data it can hold values of multiple data type 
+# X=      1,89,A,hey,0,B
+# indices 0, 1,2, 3, 4,5
+
+
+
+
+
+#creating list :
+#int list 
+num=[1,2,3,4,4]
+print(type(num))
+print(num)
+
+mix = ["fahim","mahir",1,6]
+print(mix)
+print(type(mix))
+
+
+
+#matrix 
+
+mat =[['a','b'],[1,2]]
+print(mat)
+print(type(mat))
