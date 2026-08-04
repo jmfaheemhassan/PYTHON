2b428d29dfa5814cf099fde45981f0ae42893955
jmfahimhasan <pu66cse1079@primeuniversity.edu.bd>
2026-08-04 23:24:35 +0600

    age&sum

---
+++ age&sum.py
@@ -0,0 +1,14 @@
+age=10
+if age>18:
+    print("you are adult")
+else:
+    print("your are not adult")
+
+
+
+
+
+    num1=10
+    num2=20
+    sum=num1+num2
+    print("Sum of {0} {1} is {2}".format(num1,num2,sum))
